d36e3b8f24a985f0257aa6b94d3c8740641861c0
Eric Klavins <klavins@uw.edu>
2025-04-22 14:54:58 -0700

    notes

--- MatrixAnalysis/Book/1.2.lean
+++ MatrixAnalysis/Book/1.2.lean
@@ -4,21 +4,40 @@ import MatrixAnalysis.Data.Matrix.Eigenvalues
 
 namespace MatrixAnalysis
 
+/- Note: In Mathlib the characteristic polynomial is Matrix.charpoly. We should probably use that here. -/
+
+namespace Temp
+
+open Polynomial
+
+noncomputable
+def pre_cp {n:ℕ} (A : Matrix (Fin n) (Fin n) ℂ) :=
+  (X * (C (1:Matrix (Fin n) (Fin n) ℂ)) - C A)
+
+def M : Matrix (Fin 2) (Fin 2) ℂ := Matrix.of !![1,2;0,4]
+variable (t:ℂ)
+#check pre_cp M t -- It's a polynomial over matrices. But it's also a matrix,
+                  -- so who do we take its determinant?
+
+end Temp
+
+-/
+
+variable {n:ℕ} {A : Matrix (Fin n) (Fin n) ℂ}
+
 /- The characteristic polynomial of a matrix A is |sI-A|. -/
 
-def char_poly {n:ℕ} (A : Matrix (Fin n) (Fin n) ℂ) (s:ℂ) := (s•1 - A).det
+def char_poly (A : Matrix (Fin n) (Fin n) ℂ) (s:ℂ) := (s•1 - A).det
 
 /- # Observation 1.2.4a
    The characteristic polynomial is degree (at most) n -/
 
-theorem char_poly_coeff {n:ℕ} {A : Matrix (Fin n) (Fin n) ℂ} {s:ℂ}
-  : ∃ p : Poly ℂ n, p.apply s = char_poly A s := sorry
+theorem char_poly_coeff (s:ℂ) : ∃ p : Poly ℂ n, p.apply s = char_poly A s := sorry
 
 /- # Observation 1.2.4b
    The roots of the characteristic polynomial form the spectrum of A -/
 
-theorem cp_to_spectrum {n:ℕ} {A : Matrix (Fin n) (Fin n) ℂ}
-  : spectrum A = { s | char_poly A s = 0 } := by
+theorem cp_to_spectrum : spectrum A = { s | char_poly A s = 0 } := by
   simp_all[spectrum,char_poly,is_eigenvalue]
   apply Set.eq_of_subset_of_subset
   . intro s hs
@@ -28,5 +47,17 @@ theorem cp_to_spectrum {n:ℕ} {A : Matrix (Fin n) (Fin n) ℂ}
   . intro s hs
     simp_all
     sorry
+
+
+
+/- # Exercise p 39 : Show the characteristic polynomial of a diagonal matrix is what you'd expect. -/
+
+theorem cp_of_diagonal (s:ℂ) : Matrix.IsDiag A → char_poly A s = := sorry
+
+
+
+
+
+
 
 end MatrixAnalysis
